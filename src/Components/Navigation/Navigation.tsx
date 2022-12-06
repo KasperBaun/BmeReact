bdeac434ecb72a1d24ca4362824f658014a7d05a
Kasper Baun <kasperbaun@hotmail.com>
2022-12-06 19:45:02 +0100

    storageService

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,8 +1,9 @@
 import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigation } from "../../Models/Navigation";
 import MailIcon from '@mui/icons-material/Mail';
 import { DrawerStyles } from "./../../Styles/Drawer"
+import { StorageService } from "../../Repositories/StorageService";
 
 export interface INavigationProps {
     elements: Navigation[]
@@ -10,8 +11,29 @@ export interface INavigationProps {
 }
 
 export const NavMenu: React.FC<INavigationProps> = function Navigation(props: INavigationProps) {
+    var stor: StorageService;
+    useEffect(
+        () => {
+            stor = StorageService.GetStorageService();
+        }
+        , []);
+
+    function handleOnClick(){
+        stor.testCache();
+    };
+
+    function showCache(){
+        console.log("Current cache size: " + stor.storageSize +" kB");
+        //console.log("Size of item in localStorage: " + new Blob([localStorage.getItem("somekey1")!]).size);
+    }
+
+    function printCurrentDate(){
+        console.log("Date.now() " + Date.now());
+        console.log("new Date().getUTCDate() " + new Date().getUTCDate());
+    }
+
     return (
-        <Box sx={{ display: 'flex', width: 250}}  >
+        <Box sx={{ display: 'flex', width: 250 }}  >
             <Drawer
                 sx={{
                     width: DrawerStyles.width,
@@ -20,12 +42,12 @@ export const NavMenu: React.FC<INavigationProps> = function Navigation(props: IN
                         width: DrawerStyles.width,
                         boxSizing: 'border-box'
                     },
-                    
+
                 }}
                 variant="permanent"
                 anchor="left"
             >
-                <List sx={{background: (theme) => theme.palette.background.default}}>
+                <List sx={{ background: (theme) => theme.palette.background.default }}>
                     {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => {
                         return (
                             <ListItem key={text} disablePadding>
@@ -41,7 +63,31 @@ export const NavMenu: React.FC<INavigationProps> = function Navigation(props: IN
                 </List>
                 <Divider />
                 <List>
-                    {['All mail', 'Trash', 'Spam'].map((text) => (
+                    <ListItem key={"FillCache"} disablePadding>
+                        <ListItemButton onClick={handleOnClick}>
+                            <ListItemIcon>
+                                <MailIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"FillCache"} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem key={"ShowCache"} disablePadding>
+                        <ListItemButton onClick={showCache}>
+                            <ListItemIcon>
+                                <MailIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"ShowCache"} />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem key={"PrintCurrentDate"} disablePadding>
+                        <ListItemButton onClick={printCurrentDate}>
+                            <ListItemIcon>
+                                <MailIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={"PrintCurrentDate"} />
+                        </ListItemButton>
+                    </ListItem>
+                    {/* {['All mail', 'Trash', 'Spam'].map((text) => (
                         <ListItem key={text} disablePadding>
                             <ListItemButton>
                                 <ListItemIcon>
@@ -50,7 +96,7 @@ export const NavMenu: React.FC<INavigationProps> = function Navigation(props: IN
                                 <ListItemText primary={text} />
                             </ListItemButton>
                         </ListItem>
-                    ))}
+                    ))} */}
                 </List>
             </Drawer>
         </Box>
---
+++ src/Repositories/StorageService.ts
@@ -0,0 +1,161 @@
+//import { Constants } from "../helpers/constants";
+
+export class StorageService implements Storage {
+    private static instance: StorageService;
+    public storageSize: number = 0;
+    private currentDeviceLocalStorageQuota: number = 0;
+    private keyCreationTimeMap = new Map<string, number>;
+    private baseStorage: Storage;
+    private prefix: string = "ATPEPCF";
+    length: number = 0;
+    //[name: string]: any;
+    public static cacheLocation = "localStorage"
+    static GetStorageService() {
+        if (this.instance) {
+            return this.instance;
+        } else {
+            this.instance = new StorageService(localStorage);
+            return this.instance;
+        }
+    }
+
+
+    constructor(baseStorage: Storage) {
+        this.baseStorage = baseStorage;
+        this.currentDeviceLocalStorageQuota = this.getLocalStorageQuotaInBytes();
+        console.log("StorageService initialised with " + (this.currentDeviceLocalStorageQuota / 1000) + " kB of local storage");
+        this.getCacheStatus();
+       
+    }
+    public testCache() {
+        console.log("Testing cache");
+        let i: number = 0;
+        do {
+            try {
+                let value: string = "somevalue" + i;
+                let key: string = "somekey" + i;
+                this.setItem(key, value, true);
+                i++;
+            } catch (e) {
+            }
+
+        }
+        while (this.storageSize < this.currentDeviceLocalStorageQuota * 0.75)
+
+    }
+    public getCacheStatus(){
+        console.log("Current StorageService cache status: " + (this.storageSize/this.currentDeviceLocalStorageQuota)*100 + "%")
+        console.log("baseStorage.length: " + this.baseStorage.length);
+        console.log("this.storageSize: " + this.storageSize);
+        console.log("keyDateMap.count: "+ this.keyCreationTimeMap.size)
+    }
+    public clear(): void {
+        this.length = this.baseStorage.length;
+        for (let i = 0; i < this.baseStorage.length; i++) {
+            const key = this.baseStorage.key(i);
+            if (key?.startsWith(this.prefix)) {
+                this.baseStorage.removeItem(key);
+            }
+        }
+        this.length = this.baseStorage.length;
+    }
+    public getItem(key: string): string | null {
+        this.length = this.baseStorage.length;
+        return this.baseStorage.getItem(`${this.prefix}-${key}`);
+    }
+    public key(index: number): string | null {
+        this.length = this.baseStorage.length;
+        return this.baseStorage.key(index);
+    }
+    public removeItem(key: string): void {
+        var storageObject = this.baseStorage.getItem(`${this.prefix}-${key}`);
+        this.baseStorage.removeItem(`${this.prefix}-${key}`);
+        this.length = this.baseStorage.length;
+        this.storageSize = this.storageSize + key.length + storageObject!.length;
+    }
+    public setItem(key: string, value: string, handleCache?: boolean): void {
+        this.baseStorage.setItem(`${this.prefix}-${key}`, value);
+        this.length = this.baseStorage.length;
+        this.storageSize = this.storageSize + key.length + value.length;
+        this.keyCreationTimeMap.set(
+            `${this.prefix}-${key}`,
+            Date.now()
+        );
+        if(handleCache){
+            this.handleCacheStatus();
+        }
+    }
+
+    private handleCacheStatus() {
+        if (this.storageSize > this.currentDeviceLocalStorageQuota * 0.75) {
+            console.log("cache is 75% or more full - deleting old items");
+            let oldestItem: [string, number] = ["", Date.now()];
+            this.keyCreationTimeMap.forEach((value: number, key: string) => {
+                if (oldestItem[1] > value) {
+                    oldestItem = [key, this.keyCreationTimeMap.get(key)!];
+                }
+            });
+            var key = oldestItem[0];
+            var value = oldestItem[1];
+            var itemDeleted = this.keyCreationTimeMap.delete(key);
+            if (itemDeleted) {
+                console.log("Item deleted from cache - key: " + key + " value: " + value + ".");
+                this.getCacheStatus();
+                //console.log("Current cache status: " + (this.storageSize/this.currentDeviceLocalStorageQuota)*100 + " %");
+            }
+            else{
+                console.log("Current cache status: " + (this.storageSize/this.currentDeviceLocalStorageQuota)*100 + " %");
+            }
+        }
+    }
+    private getUsedSpaceOfLocalStorageInBytes() {
+        // Returns the total number of used space (in Bytes) of the Local Storage
+        var b = 0;
+        for (var key in this.baseStorage) {
+            if (this.baseStorage.hasOwnProperty(key)) {
+                b += key.length + this.baseStorage.getItem(key)!.length;
+            }
+        }
+        return b;
+    }
+    private getUnusedSpaceOfLocalStorageInBytes() {
+        let maxByteSize = 10485760; // 10MB
+        let minByteSize = 0;
+        let tryByteSize = 0;
+        let testQuotaKey = 'testQuota';
+        let timeout = 20000;
+        let startTime = new Date().getTime();
+        let unusedSpace = 0;
+        let runtime = new Date().getTime();
+        do {
+            runtime = new Date().getTime() - startTime;
+            try {
+                tryByteSize = Math.floor((maxByteSize + minByteSize) / 2);
+                this.setItem(testQuotaKey, String('1').repeat(tryByteSize));
+                minByteSize = tryByteSize;
+            } catch (e) {
+                maxByteSize = tryByteSize - 1;
+            }
+        } while ((maxByteSize - minByteSize > 1) && runtime < timeout);
+
+        this.removeItem(testQuotaKey);
+
+        if (runtime >= timeout) {
+            console.log("Unused space calculation may be off due to timeout.");
+        }
+
+        // Compensate for the byte size of the key that was used, then subtract 1 byte because the last value of the tryByteSize threw the exception
+        unusedSpace = tryByteSize + testQuotaKey.length - 1;
+        return unusedSpace;
+    }
+
+    private getLocalStorageQuotaInBytes() {
+        // Returns the total Bytes of Local Storage Space that the browser supports
+        var unused = this.getUnusedSpaceOfLocalStorageInBytes();
+        var used = this.getUsedSpaceOfLocalStorageInBytes();
+        this.baseStorage.clear();
+        this.storageSize = 0;
+        return (unused + used);
+    }
+
+}
